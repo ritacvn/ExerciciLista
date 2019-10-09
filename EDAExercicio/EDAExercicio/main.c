d4318738f6029d9daf230b803c808d4303cfdc85
Rita de Cássia <ritadecassia.vasconcelos@hotmail.com>
2019-10-09 09:08:27 -0400

    Initial Commit
    Signed-off-by: Rita de Cássia <ritadecassia.vasconcelos@hotmail.com>

--- EDAExercicio/EDAExercicio/main.c
+++ EDAExercicio/EDAExercicio/main.c
@@ -1,10 +1,15 @@
 //
-//  main.swift
-//  EDATrabalho
+//  main.c
+//  EDAExercicio
 //
 //  Created by Rita de Cássia Vasconcelos do Nascimento on 09/10/19.
 //  Copyright © 2019 Rita de Cássia Vasconcelos do Nascimento. All rights reserved.
 //
 
+#include <stdio.h>
 
-
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
