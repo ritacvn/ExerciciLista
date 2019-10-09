5ce31dbfa03709e9a43082db95a5a4f0dfccda4e
Rita de Cássia <ritadecassia.vasconcelos@hotmail.com>
2019-10-09 08:56:07 -0400

    Initial Commit

---
+++ EDATrabalho/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EDATrabalho
+//
+//  Created by Rita de Cássia Vasconcelos do Nascimento on 09/10/19.
+//  Copyright © 2019 Rita de Cássia Vasconcelos do Nascimento. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
